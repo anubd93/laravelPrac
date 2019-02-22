06cbc43fa919f1aade373a166d1285665d57566b
anubd93 <anubd93@gmail.com>
2019-02-22 18:57:19 +0600

    :new commit 22-02-19

--- resources/views/pages/service.blade.php
+++ resources/views/pages/service.blade.php
@@ -5,9 +5,9 @@
         {{--Laravel Homestead virtual machine, so it's highly recommended that you use Homestead as your local Laravel--}}
         {{--development environment.</p>--}}
     @if(count($services)>0)
-        <ul>
+        <ul class="list-group">
             @foreach($services as $service)
-                <li>{{$service}}</li>
+                <li class="list-group-item">{{$service}}</li>
             @endforeach
         </ul>
     @endif
